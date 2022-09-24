37c0d2e73f4979b2d572cde6288cdf4a9b4017fe
Javier <andrejav@outlook.com>
2022-09-24 10:03:01 -0500

    Se agrega angular al proyecto

--- Server/demo/src/main/java/com/proyecto/angular/demo/DTO/PedidoDTO.java
+++ Server/demo/src/main/java/com/proyecto/angular/demo/DTO/PedidoDTO.java
@@ -1,10 +1,6 @@
 package com.proyecto.angular.demo.DTO;
 
 import java.sql.Date;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import com.proyecto.angular.demo.Entity.General.ClienteEntity;
 
 import lombok.AllArgsConstructor;
@@ -17,13 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoDTO {
-    
-    
-    private Integer Id ;     
-    private String Detalle ;  
+
+    private Integer Id;
+    private String Detalle;
     private Date FechaRegistro;
-    private Double Total;  
-    private Integer Estado ;  
-	private ClienteEntity cliente;
+    private Double Total;
+    private Integer Estado;
+    private ClienteEntity cliente;
 
 }
